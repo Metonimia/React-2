e400f117b0398bafaf8d2326eeb50d8460f1b0e6
Pawel Schmidt <px.sxxx@gmail.com>
2018-01-23 19:45:15 +0100

    Fixed project

--- script.js
+++ script.js
@@ -1,77 +1,92 @@
 const movies = [
 	{
 		id: 1,
-		title: 'Harry Potter',
-		desc: 'Film o czarodzieju',
-		pic: 'http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg'
+		title: "Harry Potter",
+		desc: "Film o czarodzieju",
+		pic: "http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg"
 	},
 	{
 		id: 2,
-		title: 'Król Lew',
-		desc: 'Film o królu sawanny',
-		pic: 'http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg'
+		title: "Król Lew",
+		desc: "Film o królu sawanny",
+		pic: "http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg"
 	},
 	{
 		id: 3,
-		title: 'Wielkie piękno',
-		desc: 'Trzeba zobaczyć, żeby wiedzieć',
-		pic: 'http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg'
+		title: "Wielkie piękno",
+		desc: "Trzeba zobaczyć, żeby wiedzieć",
+		pic: "http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg"
 	},
 	{
 		id: 4,
-		title: 'Paterson',
-		desc: 'Film o kierowcy autobusu i poecie',
-		pic: 'http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg'
+		title: "Paterson",
+		desc: "Film o kierowcy autobusu i poecie",
+		pic: "http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg"
 	},
 	{
 		id: 5,
-		title: 'Lady Bird',
-		desc: 'Jutro się dowiem :)',
-		pic: 'http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg'
+		title: "Lady Bird",
+		desc: "Jutro się dowiem :)",
+		pic: "http://harrypotterfanzone.com/wp-content/2015/07/philosophers-stone-theatrical-poster.jpg"
 	}
 ];
 
 var Movie = React.createClass({
-
 	propTypes: {
-    	movies: React.PropTypes.object.isRequired,
+		movie: React.PropTypes.object.isRequired
 	},
 
 	render: function() {
-    	return (
-	      	React.createElement('div', {},
-		        React.createElement('h1', {}, 'Lista filmów'),
-		        React.createElement('ul', {},
-		        	React.createElement('li', {key: movies.id})
-	        	)
-	    	)
-	    )
-	  },
+		return React.createElement("div", {},
+			React.createElement(MovieTitle, { title: this.props.movie.title }),
+			React.createElement(MovieDesc, { desc: this.props.movie.desc }),
+			React.createElement(MovieImage, { imageUrl: this.props.movie.pic })
+		);
+	}
 });
 
-var MoviesTitle = React.createClass({
+var MovieTitle = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string.isRequired
+	},
+
+	render: function() {
+		return React.createElement("p", {}, this.props.title);
+	}
+});
+
+var MovieDesc = React.createClass({
 	propTypes: {
 		desc: React.PropTypes.string.isRequired
 	},
 
 	render: function() {
-		return React.createElement('p', movies.title);
+		return React.createElement("p", {}, this.props.desc);
 	}
 });
 
-
-var MoviesDesc = React.createClass({
+var MovieImage = React.createClass({
 	propTypes: {
-		desc: React.PropTypes.string.isRequired
+		imageUrl: React.PropTypes.string.isRequired
 	},
 
 	render: function() {
-		return React.createElement('p', movies.desc);
+		return React.createElement("img", { src: this.props.imageUrl });
 	}
 });
 
+var MoviesList = React.createClass({
+	propTypes: {
+		items: React.PropTypes.array.isRequired
+	},
 
-var element = React.createElement(Movie, MoviesTitle, MoviesDesc, {});
+	render: function() {
+		var moviesElements = this.props.items.map(function(item) {
+			return React.createElement(Movie, { movie: item });
+		});
+		return React.createElement("ul", {}, moviesElements);
+	}
+});
+
+var element = React.createElement(MoviesList, { items: movies });
 ReactDOM.render(element, document.getElementById("app"));
-
-
